097c8412418ba892f6b692a929c2086f052a122e
Jürgen Key <sgjfsf@hotmail.com>
2019-04-02 08:40:08 +0200

    some comments added

--- determinentptimesource.sh
+++ determinentptimesource.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+#reads the ntp peer from the output of the ntpq command
 ntpq -p | grep "*" | cut -d " " -f 1 | cut -d "*" -f 2
 
--- helpers/install_ntp_for_monitoring_18.04.sh
+++ helpers/install_ntp_for_monitoring_18.04.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# this is a helper for installing the ntp client on ubuntu 18.04
+# needed for determinentptimesource.sh and ntp.conf
 timedatectl set-ntp no
 apt-install ntp
 echo "add line server 192.168.10.2 right before pool definitions!"
